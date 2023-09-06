6d57b1c09e6d12dcdc22688a727577c826f06cf9
mahmoudali94 <eng.mahmoudali94@gmail.com>
2023-09-06 13:04:03 -0700

    fix error

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for latter in range(97, 123):
-    if chr(latter) is not "q" and chr(latter) is not "e":
+    if chr(latter) != "q" and chr(latter) != "e":
         print("{}".format(chr(latter)), end="")
