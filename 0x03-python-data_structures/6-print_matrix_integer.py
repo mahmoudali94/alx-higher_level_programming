b8f4a5196dfa3675efd728f89b93863d91b15be1
mahmoudali94 <eng.mahmoudali94@gmail.com>
2023-09-11 15:37:47 -0700

    print matrix

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    if matrix:
+        for item in matrix:
+            for num in item:
+                print("{}".format(num), end=" ")
+            print()
