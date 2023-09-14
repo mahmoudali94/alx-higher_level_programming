9fc9c4040d34eef71839fd4dafe1fcf37853bd4e
mahmoudali94 <eng.mahmoudali94@gmail.com>
2023-09-13 18:24:47 -0700

    print args

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -9,5 +9,5 @@ if __name__ == "__main__":
     else:
         print("{} arguments:".format(num))
 
-    for i in range(0, num):
+    for i in range(1, num):
         print("{}: {}".format(i, sys.argv[i]))
